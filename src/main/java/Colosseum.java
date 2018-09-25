bee73e48b90689892fcd8c3ff3872c1456258b08
anisha <anishan2@illinois.edu>
2018-09-25 11:45:58 -0500

    finished activity for lab

--- src/main/java/Colosseum.java
+++ src/main/java/Colosseum.java
@@ -90,7 +90,13 @@ public class Colosseum {
      * Implement this function.
      */
     public static void printWhoIsAhead() {
-        System.out.println("Implement me!");
+        if (firstPokemon.hitPoints > secondPokemon.hitPoints) {
+            System.out.println(firstPokemon.name + "is currently ahead!");
+        } else if (firstPokemon.hitPoints < secondPokemon.hitPoints) {
+            System.out.println(secondPokemon.name + "is currently ahead!");
+        } else {
+            System.out.println("Tie!");
+        }
     }
 
     /**
@@ -101,7 +107,11 @@ public class Colosseum {
      * Write this function.
      */
     public static void determineWinner() {
-        System.out.println("Implement me!");
+        if (firstPokemon.hitPoints > secondPokemon.hitPoints) {
+            System.out.println(firstPokemon.name + " won!");
+        } else {
+            System.out.println(secondPokemon.name + " won!");
+        }
     }
 
     /**
